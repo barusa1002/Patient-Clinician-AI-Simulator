411ae8e82cb420fee7861bc42757f979d5b083c9
barusa1002 <barusa0517@gmail.com>
2026-04-02 16:29:23 +0900

    Create db.py

---
+++ db.py
@@ -0,0 +1,10 @@
+from supabase import create_client
+import streamlit as st
+
+@st.cache_resource
+def get_supabase():
+    url = st.secrets["SUPABASE_URL"]
+    key = st.secrets["SUPABASE_KEY"]
+    return create_client(url, key)
+
+supabase = get_supabase()
